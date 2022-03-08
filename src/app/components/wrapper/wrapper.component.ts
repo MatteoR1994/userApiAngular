233534f89eda01331306844aec67ab3ac67210c8
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-03-08 17:31:49 +0100

    implementato css scheda utente e fatte fatch normali e con un servizio

--- src/app/components/wrapper/wrapper.component.ts
+++ src/app/components/wrapper/wrapper.component.ts
@@ -3,6 +3,7 @@ import { Address } from 'src/app/model/address';
 import { Company } from 'src/app/model/company';
 import { Geo } from 'src/app/model/geo';
 import { User } from 'src/app/model/user';
+import { DataServiceService } from 'src/app/services/data-service.service';
 import { UserComponent } from '../user/user.component';
 
 @Component({
@@ -14,63 +15,74 @@ export class WrapperComponent implements OnInit {
 
   userList: User[] = [];
 
-  constructor() { 
-    this.fetchUserFromApi();
+  constructor(private dataService: DataServiceService) { 
+    //this.fetchUserFromApi();
+    
+    // dataService.fetchtUsers().then(value => console.log(value));
+    this.dataService.fetchtUsers().then(value => this.userList = value);
   }
 
   ngOnInit(): void {
-    
   }
 
-  fetchUserFromApi() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((json) => this.createUser(json));
-  }
+  // 1 - UTILIZZANDO LA PROPRIETA' DELLE INTEFRACCE
+  // fetchUserFromApi() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then((response) => response.json())
+  //     .then((data) => this.userList = data);
+  // }
 
-  createUser(data: any) {
-    for (const element of data) {
-      const newUSer: User = {
-        id: element.id,
-        name: element.name,
-        username: element.username,
-        email: element.email,
-        phone: element.phone,
-        website: element.website,
-        address: this.createAddress(element.address),
-        company: this.createCompany(element.company)
-      };
-      this.userList.push(newUSer);      
-    }
-    console.log('Array creato', this.userList);
-  }
 
-  createAddress(element: any): any {
-    const newAddress: Address = {
-      street: element.street,
-      suite: element.suite,
-      city: element.city,
-      zipcode: element.zipcode,
-      geo: this.createGeo(element.geo)
-    };
-    return newAddress;
-  }
+  // 2 - UTILIZZANDO LE INTEFRACCE COME OGGETTI CLASSE
+  // fetchUserFromApi() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then((response) => response.json())
+  //     .then((json) => this.createUser(json));
+  // }
 
-  createGeo(element: any): any {
-    const newGeo: Geo = {
-      lat: element.lat,
-      lng: element.lng
-    };
-    return newGeo;
-  }
+  // createUser(data: any) {
+  //   for (const element of data) {
+  //     const newUSer: User = {
+  //       id: element.id,
+  //       name: element.name,
+  //       username: element.username,
+  //       email: element.email,
+  //       phone: element.phone,
+  //       website: element.website,
+  //       address: this.createAddress(element.address),
+  //       company: this.createCompany(element.company)
+  //     };
+  //     this.userList.push(newUSer);      
+  //   }
+  //   console.log('Array creato', this.userList);
+  // }
 
-  createCompany(element: any): any {
-    const newCompany: Company = {
-      name: element.name,
-      catchPhrase: element.catchPhrase,
-      bs: element.bs
-    };
-    return newCompany;
-  }
+  // createAddress(element: any): any {
+  //   const newAddress: Address = {
+  //     street: element.street,
+  //     suite: element.suite,
+  //     city: element.city,
+  //     zipcode: element.zipcode,
+  //     geo: this.createGeo(element.geo)
+  //   };
+  //   return newAddress;
+  // }
+
+  // createGeo(element: any): any {
+  //   const newGeo: Geo = {
+  //     lat: element.lat,
+  //     lng: element.lng
+  //   };
+  //   return newGeo;
+  // }
+
+  // createCompany(element: any): any {
+  //   const newCompany: Company = {
+  //     name: element.name,
+  //     catchPhrase: element.catchPhrase,
+  //     bs: element.bs
+  //   };
+  //   return newCompany;
+  // }
 
 }
---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { Address } from '../model/address';
+import { Company } from '../model/company';
+import { Geo } from '../model/geo';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  uList: User[] = [];
+
+  constructor() { }
+
+  // fetchtUsers(): Promise<User[]> {
+  //   return fetch('https://jsonplaceholder.typicode.com/users')
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         this.uList = data;
+  //         return this.uList;
+  //       });
+  // }
+
+  fetchtUsers(): Promise<User[]> {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        return this.createUser(data);
+      });
+  }
+
+  createUser(data: any): User[] {
+    const userList: User[] = [];
+    for (const element of data) {
+      const newUSer: User = {
+        id: element.id,
+        name: element.name,
+        username: element.username,
+        email: element.email,
+        phone: element.phone,
+        website: element.website,
+        address: this.createAddress(element.address),
+        company: this.createCompany(element.company)
+      };
+      userList.push(newUSer);      
+    }
+    //this.uList = userList;
+    return userList;
+  }
+
+  createAddress(element: any): any {
+    const newAddress: Address = {
+      street: element.street,
+      suite: element.suite,
+      city: element.city,
+      zipcode: element.zipcode,
+      geo: this.createGeo(element.geo)
+    };
+    return newAddress;
+  }
+
+  createGeo(element: any): any {
+    const newGeo: Geo = {
+      lat: element.lat,
+      lng: element.lng
+    };
+    return newGeo;
+  }
+
+  createCompany(element: any): any {
+    const newCompany: Company = {
+      name: element.name,
+      catchPhrase: element.catchPhrase,
+      bs: element.bs
+    };
+    return newCompany;
+  }
+
+}
